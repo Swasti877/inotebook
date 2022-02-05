ae3d4cdb32859ef655244902cf9683dc2f5377ba
Swasti877 <bhagorasurekhaben@gmail.com>
2022-02-05 10:45:12 +0530

    1.0.2
    Finish most of backend which include endpoints of note, auth

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -11,7 +11,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
         res.json(notes);
     } catch (error) {
         console.error(error);
-        res.status(500).send({error: "Internal server error"});
+        res.status(500).send({ error: "Internal server error" });
     }
 })
 
@@ -36,29 +36,54 @@ router.post('/addnotes', fetchuser, [
         res.json(savedNote);
     } catch (error) {
         console.error(error.message);
-        res.status(500).send({error: "Internal server error"});
+        res.status(500).send({ error: "Internal server error" });
     }
 })
 
 //Route: 3 For Updating the Note using PUT -> Login Required
 router.put('/fetchallnotes/:id', fetchuser, async (req, res) => {
-    const {title, description, tag} = req.body;
-    const newNote = {};
-    if(title){newNote.title = title};
-    if(description){newNote.description = description};
-    if(tag){newNote.tag = tag};
+    try {
+        const { title, description, tag } = req.body;
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
 
-    //check wheather the Note exist or Not
-    let note = await Notes.findById(req.params.id);
-    if(!note){return res.status(404).send({error: "File Not Found."})}
+        //check wheather the Note exist or Not
+        let note = await Notes.findById(req.params.id);
+        if (!note) { return res.status(404).send({ error: "File Not Found." }) }
 
-    //check wheather the user is one who can access?
-    if(note.user.toString() !== req.user.id){
-        return res.status(401).send("Not Allowed");
+        //check wheather the user is one who can access?
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json(note);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Internal server error" });
     }
+})
 
-    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
-    res.json(note);
+//Route: 4 For Deleting the Notes using DELETE-> Login Required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    try {
+        //check wheather the note exist for not.
+        let note = await Notes.findById(req.params.id);
+        if (!note) { return res.status(404).send('File not Found') }
+
+        //check wheather the user is same to whom the note belong.
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send('Not Allowed')
+        }
+
+        note = await Notes.findByIdAndDelete(req.params.id)
+        res.send({ Success: 'File has been Deleted.', note: note })
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Internal server error" });
+    }
 })
 
 module.exports = router;
